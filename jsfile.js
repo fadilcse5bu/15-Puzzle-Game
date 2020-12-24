dbbbf020b51b4000523503d1e69ae88aaff36302
fadilcse5bu <60518662+fadilcse5bu@users.noreply.github.com>
2020-12-24 10:08:33 +0600

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,18 @@
+// var array = JSON.parse(localStorage.getItem('array')) || []
+
+// const change = document.getElementById('btn')
+// change.addEventListener("click", () => {
+//   const item = {
+//     id: "1",
+//     name: "Saidul"
+//   }
+//   array.push(item)
+//   localStorage.setItem("array", JSON.stringify(array))
+// })
+
+// const items = JSON.parse(localStorage.getItem("array"))
+// console.log(array)
+
+// var ar = [1, 2, 3, 4, 5]
+// ar.splice(2, 3)
+// console.log(ar)
